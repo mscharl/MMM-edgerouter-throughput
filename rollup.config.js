aa98b146709942eeadf5c7d64b2d74f6bfcccbb1
Michael Scharl <michael.scharl@massiveart.com>
2019-07-27 11:19:04 +0200

    Added automatic banner

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,21 @@
+import banner from 'rollup-plugin-banner';
 import typescript from 'rollup-plugin-typescript';
+
+const bannerText = [
+    'EdgeRouter Throughput',
+    '<%= pkg.description %>',
+    '',
+    'Version <%= pkg.version %>',
+    'By <%= pkg.author %>',
+    '',
+    'License <%= pkg.license %>',
+].join('\n');
 
 export default [{
     input: './MMM-edgerouter-throughput.ts',
     plugins: [
-        typescript()
+        typescript(),
+        banner(bannerText),
     ],
     output: {
         file: 'MMM-edgerouter-throughput.js',
